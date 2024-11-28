fdf6eb173de31ea63155295303cff7fd1ff489a0
Adrián Perogil Fernández <apchopito@gmail.com>
2024-11-28 11:57:21 +0100

    Changes in header section

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -2,6 +2,30 @@ function Header() {
     return (
       <>
         <h1>AQUI EL HEADER</h1>
+        <header className="header_wrapped">
+          <div className="header_layout">
+            <div className="header_layout__buttons">
+              <button>
+                
+              </button>
+              <button>
+                
+                </button>
+                <button>
+                
+                </button>
+                <button>
+                
+                </button>
+                <button>
+                
+                </button>
+                <button>
+                
+                </button>
+            </div>
+          </div>
+        </header>
       </>
     )
   }
